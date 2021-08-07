f386910712735a17677879f16c687dbc99501a7e
bharatha1017 <wubharatha@gmail.com>
2021-08-07 07:50:59 +0530

    Services Page Modified

--- HARSHANA-MEDICARE-FRONTEND/src/components/pages/Services.js
+++ HARSHANA-MEDICARE-FRONTEND/src/components/pages/Services.js
@@ -7,24 +7,29 @@ export default function Services() {
       <div class="box">
         <div>
           <ServiceCardL
-            text1='SERVICE 1'
-            text2='IMAGE'
+            url='images/img-9.jpg'
+            text='IMAGE 1'
+            heading='Provide lab Consaltation Services'
           />
           <ServiceCardL
-            text1='SERVICE 2'
-            text2='IMAGE'
+            url='images/img-2.jpg'
+            text='IMAGE 2'
+            heading='Channel Your Doctor fast and Easy with Doc-Support'
           />
           <ServiceCardL
-            text1='SERVICE 3'
-            text2='IMAGE'
+            url='images/img-3.jpg'
+            text='IMAGE 3'
+            heading='Support Your Health and medical needs Effectively'
           />
           <ServiceCardL
-            text1='SERVICE 4'
-            text2='IMAGE'
+            url='images/img-4.jpg'
+            text='IMAGE 4'
+            heading='Buy new Medicine from Our Store'
           />
           <ServiceCardL
-            text1='SERVICE 5'
-            text2='IMAGE'
+            url='images/img-8.jpg'
+            text='IMAGE 5'
+            heading='Sign-in with our Shop For Meny More services'
           />
         </div>
           
