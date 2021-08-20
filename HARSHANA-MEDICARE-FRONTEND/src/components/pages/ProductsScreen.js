165c71cb6275dbdd800fabeaf603f59e75c963d6
Sachin Supunthaka <sachinsupunthaka96@gmail.com>
2021-08-20 15:19:24 +0530

    fix:screenfix

--- HARSHANA-MEDICARE-FRONTEND/src/components/pages/ProductsScreen.js
+++ HARSHANA-MEDICARE-FRONTEND/src/components/pages/ProductsScreen.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import axios from "axios";
 
 import Product from "../parts/Products";
@@ -6,20 +6,31 @@ import Product from "../parts/Products";
 import "../../App.css";
 import "../parts/Products.css";
 
-class ProductScreen extends Component {
-  state = {
-    products: [],
-  };
 
-  
+
+function ProductScreen()  {
+  // state = {
+  //   products: [],
+  // };
+
+  var _isInit = true;
+
+  const[products, setProducts] = useState([]);
+
+
+
+  useEffect({
+    console
+  },[])
 
   componentDidMount() {
     console.log("[screen:product.js] componentDidMount");
 
     axios.get("http://localhost:5000/api/products").then((response) => {
-      this.setState({
-        products: response.data,
-      });
+      // this.setState({
+      //   products: response.data,
+      // });
+      products.push(response.data);
       console.log(response);
     });
   }
