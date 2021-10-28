bf7e3451c582e3238cf19cd01d153184b81fd54e
Sachin Supunthaka <sachinsupunthaka96@gmail.com>
2021-10-28 11:37:52 +0530

    feat:cartScreenIniti

---
+++ HARSHANA-MEDICARE-FRONTEND/src/components/pages/cartScreen.js
@@ -0,0 +1,6 @@
+export default function cartScreen(props) {
+  const productId = props.match.params.id;
+  const qty = props.location.search
+    ? Number(props.location.search.split("=")[1])
+    : 1;
+}
