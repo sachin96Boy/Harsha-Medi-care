51909505fa3ef475c9cd516b23269e6438163ff0
Sachin Supunthaka <sachinsupunthaka96@gmail.com>
2021-11-01 23:25:33 +0530

    feat:productScheminit

---
+++ backend/models/productModel.js
@@ -0,0 +1,22 @@
+import { mongoose } from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    category: { type: String, required: true },
+    image: { type: String, required: true },
+    price: { type: Number, required: true },
+    countInStock: { type: Number, required: true },
+    brand: { type: String, required: true },
+    rating: { type: Number, required: true },
+    numReviews: { type: Number, required: true },
+    description: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const product = mongoose.model("product", productSchema);
+
+export default product;
