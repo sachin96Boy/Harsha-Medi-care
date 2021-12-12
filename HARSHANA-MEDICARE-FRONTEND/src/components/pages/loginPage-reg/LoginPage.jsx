9e8a915e96e782931196e95c3157dba2db419ac9
bharatha1017 <wubharatha@gmail.com>
2021-12-12 12:41:39 +0530

    sign in page

--- HARSHANA-MEDICARE-FRONTEND/src/components/pages/loginPage-reg/LoginPage.jsx
+++ HARSHANA-MEDICARE-FRONTEND/src/components/pages/loginPage-reg/LoginPage.jsx
@@ -1,31 +1,34 @@
 import React from "react";
 
+import './LoginPage.css'
+
 export default function LoginPage() {
   return (
-    <div className="loginPage">
-      <dev className="loginPagewrapper">
-        <div className="logo">
-          <img
-            className="medicare-logo"
-            src={"#insert src here"}
-            alt={"loginLogo"}
-          ></img>
-          <h3 className="Log-in">Log-In</h3>
+    <div className="body1">
+      <h2>Login Form</h2>
+      <form className="form1">
+        <div className="imgcontainer">
+          <img src={"images/logo/harshLogo1.jpg"} alt={"loginLogo"} className="medicare-logo"></img>
         </div>
-        <div className="formController">
-          <form>
-            <label>UserName:</label>
-            <input type="text" alt={"UserName"} />
-            <label>Password:</label>
-            <input type="text" alt={"Password"} />
 
-            <input className="customInputButton" type="Submit" value="Log In" />
-          </form>
-          <div className="newSign-up">
-              <h3>Sign UP Insted</h3>
-          </div>
+        <div className="container1">
+          <label for="uname"><b>Username</b></label>
+          <input type="text" placeholder="Enter Username" name="uname" required className="input1"/>
+
+          <label for="psw"><b>Password</b></label>
+          <input type="password" placeholder="Enter Password" name="psw" required className="input1"/>
+        
+          <button type="submit" className="button1">Login</button>
+          <label>
+            <input type="checkbox" checked="checked" name="remember"/> Remember me
+          </label>
         </div>
-      </dev>
+
+        <div className="container1">
+          <button type="button" className="cancelbtn">Cancel</button>
+          <span class="psw">Forgot <a href="#">password?</a></span>
+        </div>
+      </form>
     </div>
   );
 }
